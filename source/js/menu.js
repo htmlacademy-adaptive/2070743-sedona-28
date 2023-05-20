0303fe7866bd913cafb4a0974a9cf53f2193b82a
Sofya Yenkova <enkovasofi@yandex.ru>
2023-05-20 18:40:15 +0300

    дорабатывает проект по критериям

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,6 +3,9 @@ let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
+navMain.classList.add('main-nav--closed');
+navMain.classList.remove('main-nav--opened');
+
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
